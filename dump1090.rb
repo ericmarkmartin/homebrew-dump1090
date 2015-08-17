a3230b2381dbc6423e68767d25b4516b40d0a5fa
Rick Pontefract <rjpontefract@gmail.com>
2015-08-17 13:33:58 +1200

    Initial commit

---
+++ dump1090.rb
@@ -0,0 +1,16 @@
+require "formula"
+
+class GrDump1090 < Formula
+  homepage "https://github.com/MalcolmRobb/dump1090"
+  head "https://github.com/MalcolmRobb/dump1090.git"
+  sha1 "4c57a2efac21c51bd9001d8b8d6d6b522e79a6a2"
+  
+  def install
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
