7df9a1cec5197deba36b9c5483699c0b4bbd551f
Rick Pontefract <rjpontefract@gmail.com>
2015-08-17 19:35:14 +1200

    Add more dependencies

--- dump1090.rb
+++ dump1090.rb
@@ -5,6 +5,8 @@ class Dump1090 < Formula
   head "https://github.com/MalcolmRobb/dump1090.git"
 
   depends_on 'pkg-config' => :build
+  depends on 'libusb'
+  depends on 'librtlsdr'
   
   def install
     system "make"
