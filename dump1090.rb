7e80cdc656641366ce962315b55724586bf0dd60
Rick Pontefract <rjpontefract@gmail.com>
2015-08-17 13:54:42 +1200

    Correct the class name

--- dump1090.rb
+++ dump1090.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class GrDump1090 < Formula
+class Dump1090 < Formula
   homepage "https://github.com/MalcolmRobb/dump1090"
   head "https://github.com/MalcolmRobb/dump1090.git"
   sha1 "4c57a2efac21c51bd9001d8b8d6d6b522e79a6a2"
