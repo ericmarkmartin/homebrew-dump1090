96db4eff31ac8973f60e352674b723edfa0b367a
ericmarkmartin <ericmarkmartin@gmail.com>
2018-02-02 01:51:35 -0800

    Change unecessary double quotes to single quotes

--- dump1090.rb
+++ dump1090.rb
@@ -1,18 +1,18 @@
 class Dump1090 < Formula
-  homepage "https://github.com/MalcolmRobb/dump1090"
-  head "https://github.com/MalcolmRobb/dump1090.git"
+  homepage 'https://github.com/MalcolmRobb/dump1090'
+  head 'https://github.com/MalcolmRobb/dump1090.git'
 
   depends_on 'pkg-config' => :build
   depends_on 'libusb'
   depends_on 'librtlsdr'
   
   def install
-    system "make"
-    bin.install "dump1090"
-    bin.install "view1090"
+    system 'make'
+    bin.install 'dump1090'
+    bin.install 'view1090'
   end
 
   test do
-    system "false"
+    system 'false'
   end
 end
